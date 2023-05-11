837fae65bcd71656cc77f73e2f7ed67ed7951d25
Piotrek <piotr.blaszkowski@winlocal.com>
2023-05-11 18:42:19 +0200

    onlinegame update

--- src/main/java/com/blapiter/onlinegame/GameServiceImpl.java
+++ src/main/java/com/blapiter/onlinegame/GameServiceImpl.java
@@ -15,44 +15,29 @@ public class GameServiceImpl implements GameService {
     @Override
     public List<ArrayList<Clan>> getPrioritizeClans(Players players) {
         Clan next = null;
+        Iterator<Clan> it = null;
         Integer currentPlayers = 0;
         List<Clan> toRemove = new LinkedList<Clan>();
         List<ArrayList<Clan>> order = new ArrayList<ArrayList<Clan>>();
         Collections.sort(players.clans());
         while (!players.clans().isEmpty()) {
-
             ArrayList<Clan> clans = new ArrayList<Clan>();
-
-            next = players.clans().get(0);
-            players.clans().remove(0);
-
-            currentPlayers = next.numberOfPlayers;
-            clans.add(next);
-
-            if (currentPlayers >= players.groupCount()) {
-                order.add(clans);
-                continue;
-            }
-
-            Iterator<Clan> it = players.clans().iterator();
+            it = players.clans().iterator();
             while (it.hasNext()) {
-
                 next = it.next();
-
                 if (next.numberOfPlayers + currentPlayers <= players.groupCount()) {
                     clans.add(next);
                     toRemove.add(next);
                     currentPlayers += next.numberOfPlayers;
                 }
-
                 if (currentPlayers >= players.groupCount()) {
                     break;
                 }
             }
-
             order.add(clans);
             players.clans().removeAll(toRemove);
             Collections.sort(players.clans());
+            currentPlayers = 0;
             toRemove.clear();
         }
         return order;
