296846681023f1b37c7b3b87b3255d03fde6f9b3
Piotrek <piotr.blaszkowski@winlocal.com>
2023-05-13 08:49:48 +0200

    HashMap faster then TreeSet

--- src/main/java/com/blapiter/transactions/TransactionServiceImpl.java
+++ src/main/java/com/blapiter/transactions/TransactionServiceImpl.java
@@ -1,10 +1,9 @@
 package com.blapiter.transactions;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +12,19 @@ import org.springframework.stereotype.Service;
 public class TransactionServiceImpl implements TransactionService {
     @Override
     public List<Account> getSortedAccounts(ArrayList<Transaction> transactions) {
-        Map<String, Account> accounts = new TreeMap<>();
+        Map<String, Account> accounts = new HashMap<>();
         for (Transaction transaction : transactions) {
             this.addAccount(accounts, transaction.creditAccount());
             this.addAccount(accounts, transaction.debitAccount());
             this.updateCredit(accounts.get(transaction.creditAccount()), transaction);
             this.updateDebit(accounts.get(transaction.debitAccount()), transaction);
         }
-        return accounts.entrySet().stream().map(item -> item.getValue()).toList();
+        return accounts
+                .entrySet()
+                .stream()
+                .map(item -> item.getValue())
+                .sorted()
+                .toList();
     }
 
     protected void addAccount(Map<String, Account> accounts, String accountNumber) {
